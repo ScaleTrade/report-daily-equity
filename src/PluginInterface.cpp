efefad739f19619e5ba5753be67a01d404a89f0a
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-12-16 12:43:40 +0300

    Upd

--- src/PluginInterface.cpp
+++ src/PluginInterface.cpp
@@ -7,13 +7,13 @@ extern "C" void AboutReport(rapidjson::Value& request,
                             rapidjson::Document::AllocatorType& allocator,
                             CServerInterface* server) {
     response.AddMember("version", 1, allocator);
-    response.AddMember("name", Value().SetString("Daily Equity report", allocator), allocator);
+    response.AddMember("name", Value().SetString("Equity report", allocator), allocator);
     response.AddMember(
         "description",
         Value().SetString("The financial state of accounts at the end of each day. "
                           "The accounts are grouped according to their 'Comment' field value, allowing you to generate reports for customizable groups.",
                           allocator), allocator);
-    response.AddMember("type", REPORT_DAILY_GROUP_TYPE, allocator);
+    response.AddMember("type", REPORT_RANGE_GROUP_TYPE, allocator);
 }
 
 extern "C" void DestroyReport() {}
@@ -149,7 +149,7 @@ extern "C" void CreateReport(rapidjson::Value& request,
     const Node table_node = Table({}, table_props);
 
     const Node report = Column({
-        h1({text("Daily Equity Report") }),
+        h1({text("Equity Report") }),
         table_node
     });
 
